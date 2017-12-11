25d578cee276d9c157cadbb7125b77ff36275554
EduardoSantosGit <eduardosantos058@gmail.com>
2017-12-11 10:20:50 -0200

    worker

---
+++ AkkaNFe/Worker.cs
@@ -0,0 +1,23 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkkaNFe
+{
+    public class Worker : ReceiveActor
+    {
+        public Worker()
+        {
+
+        }
+
+
+        protected override void PreStart()
+        {
+            Console.WriteLine("testando");
+        }
+    }
+}
